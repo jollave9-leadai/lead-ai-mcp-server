3944a50671ac7ec14f575325e0d9e2a4d56ce401
Jonathan Ollave <jonathan.o@leadai.com.au>
2025-09-28 00:26:35 +0800

    replaced client_id to create_by

--- src/lib/helpers/utils.ts
+++ src/lib/helpers/utils.ts
@@ -235,12 +235,14 @@ export const getCustomerInformation = async (
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
+
+  // TODO: replace created_by with client_id
   const { data: customerPipeLines } = await supabase
     .from("customer_pipeline_items_with_customers")
     .select(
       "full_name,email, company, job_title, customer_status, customer_type, source, address, city, state, postal_code, country, phone_number, pipeline_stage_id"
     )
-    .eq("client_id", clientId);
+    .eq("created_by", clientId);
 
   console.log("clientId", clientId);
   console.log("customerPipeLines", customerPipeLines);
@@ -250,6 +252,9 @@ export const getCustomerInformation = async (
     threshold: 0.3, // how fuzzy (0 = exact, 1 = very fuzzy)
   });
   const [customerPipeline] = fuse.search(fullName);
+  console.log("customerPipeline", customerPipeline);
+  if (!customerPipeline) return;
+
   const { data: stage } = await supabase
     .from("pipeline_stages")
     .select("name, description, sort_order")
